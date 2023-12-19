24f5755497aaa277dd19e2bb9e1cd1354b360f5a
Filipe Andrade <filipeandrade.work@gmail.com>
2023-12-19 19:39:24 -0300

    first commit

---
+++ src/main/java/com/project/getatit/GetAtItApplication.java
@@ -0,0 +1,13 @@
+package com.project.getatit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetAtItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetAtItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/getatit/GetAtItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.getatit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetAtItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
